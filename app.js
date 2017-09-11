117f3a17f3c50d596519fcc8bcbf1bf5cf192ad8
Jari <jari.ojala@varjonvalo.fi>
2017-09-11 22:26:34 +0300

    basic server

--- app.js
+++ app.js
@@ -9,23 +9,7 @@ var app;
 function initialize() {
   app = express();
 
-  //enable reverse proxy support in Express. This causes the
-  //the "X-Forwarded-Proto" header field to be trusted so its
-  //value can be used to determine the protocol. See
-  //http://expressjs.com/api#app-settings for more details.
-  app.enable("trust proxy");
-
-  //redirect all http requests to https
-  app.all("*",function(req,res,next){
-    if(req.protocol==="http" && (unitTesting == null||unitTesting == false)) {
-      res.redirect("https://" + req.headers["host"] + req.url);
-    }
-    else {
-      next();
-    }
-  });
-
-  // root handler for route "/" to allow availability monitoring
+  // route "/"
   app.get("/", function(req,res) {
     res.send("tcg").end();
   });
